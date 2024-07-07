6cf4ff5dbab987e9fed641841c378b5df0dd2fea
Iryna Mykhailovska <bagira.irina.m@gmail.com>
2024-07-07 12:34:17 +0300

    change getAllContacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -2,7 +2,8 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
 
 export const getAllContacts = async () => {
-  return fs.readFile(PATH_DB, { encoding: 'utf8' });
+   const allContacts=fs.readFile(PATH_DB, { encoding: 'utf8' }).then((data)=>JSON.parse(data)).catch(console.error);
+   return allContacts;
 };
 
 console.log(await getAllContacts());
